e59d0310f8bbe38e125d21a24fd1029e09b7518e
CongDao2k4 <cong23122004@gmail.com>
2026-04-24 10:38:32 +0700

    feat: created schema.py with flexible attributes with both pdf and video

--- starter_code/schema.py
+++ starter_code/schema.py
@@ -10,4 +10,13 @@ class UnifiedDocument(BaseModel):
     TODO: Khai báo các trường với kiểu dữ liệu str ở dưới.
     """
     # Khai báo các trường ở đây...
+    document_id: str
+    source_type: str
+    content: str
+    
+    # Sử dụng Optional (có thể có hoặc không) và gán mặc định là None vì 
+    # trong thực tế một số file như vid2_missing_tags.json bị thiếu trường creator_name
+    author: Optional[str] = None
+    category: Optional[str] = None
+    timestamp: Optional[str] = None
     pass
